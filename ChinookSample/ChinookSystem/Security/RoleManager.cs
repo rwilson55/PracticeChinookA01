f3f9910c0d062c34452af22c016f680409e8b8c7
Reuben Wilson <rwilson55@studentmail.nait.ca>
2016-10-13 16:20:58 -0600

    Created RoleManager

---
+++ ChinookSample/ChinookSystem/Security/RoleManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.AspNet.Identity;                    //RoleManager
+using Microsoft.AspNet.Identity.EntityFramework;    //IdentityRole, RoleStore
+#endregion
+
+namespace ChinookSystem.Security
+{
+    public class RoleManager:RoleManager<IdentityRole>
+    {
+        public RoleManager():base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
+        {
+
+        }
+    }
+}

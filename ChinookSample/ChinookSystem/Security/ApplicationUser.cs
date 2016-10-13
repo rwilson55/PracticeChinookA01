b9f77d92ec833a23a1398e5128e82858fd058da3
Reuben Wilson <rwilson55@studentmail.nait.ca>
2016-10-13 16:40:52 -0600

    Add additional tracking properties for application user
    These fields can act as foreign key values into the user data

--- ChinookSample/ChinookSystem/Security/ApplicationUser.cs
+++ ChinookSample/ChinookSystem/Security/ApplicationUser.cs
@@ -13,5 +13,8 @@ namespace ChinookSystem.Security
     // You can add User data for the user by adding more properties to your User class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        // You can add User data for the user by adding more properties to your User class
+        public int? EmployeeId { get; set; }
+        public int? CustomerId { get; set; }
     }
 }

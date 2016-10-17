4a4308829f923af397d5728d5195f33e66c30735
rwilson55 <rwilson55@nait.ca>
2016-10-17 17:06:18 -0600

    Added security tables to DB, added login functionality/access levels

--- ChinookSample/ChinookSystem/Security/UserManager.cs
+++ ChinookSample/ChinookSystem/Security/UserManager.cs
@@ -17,5 +17,30 @@ namespace ChinookSystem.Security
             : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
         {
         }
+
+        //setting up the default WebMaster
+        #region Constants
+        private const string STR_DEFAULT_PASSWORD = "Pa$$word1";
+        private const string STR_USERNAME_FORMAT = "{0}.{1}";
+        private const string STR_EMAIL_FORMAT = "{0}@Chinook.ca";
+        private const string STR_WEBMASTER_USERNAME = "Webmaster";
+        #endregion
+        public void AddWebmaster()
+        {
+            if (!Users.Any(u => u.UserName.Equals(STR_WEBMASTER_USERNAME)))
+            {
+                var webMasterAccount = new ApplicationUser()
+                {
+                    UserName = STR_WEBMASTER_USERNAME,
+                    Email = string.Format(STR_EMAIL_FORMAT, STR_WEBMASTER_USERNAME)
+                };
+                //this Create command is from the inherited UserManager class
+                //this command creates a record on the security Users table (AspNetUsers)
+                this.Create(webMasterAccount, STR_DEFAULT_PASSWORD);
+                //this AddToRole command is from the inherited UserManager class
+                //this command creates a record on the security UserRole table (AspNetUserRoles)
+                this.AddToRole(webMasterAccount.Id, SecurityRoles.WebsiteAdmins);
+            }
+        }
     }
 }

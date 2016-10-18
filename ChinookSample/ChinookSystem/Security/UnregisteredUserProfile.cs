ba9faa27289371b61e1dea3918e5ffc083422346
rwilson55 <rwilson55@nait.ca>
2016-10-18 16:08:19 -0600

    Setup of profile datasets used to carry webpage data

---
+++ ChinookSample/ChinookSystem/Security/UnregisteredUserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.Security
+{
+    public enum UnregisteredUserType { Undefined, Employee, Customer }
+    public class UnregisteredUserProfile
+    {
+        public string UserId { get; set; } //generated
+        public string UserName { get; set; } //collected
+        public string Email { get; set; } //collected
+        public string FirstName { get; set; } //comes from user table
+        public string LastName { get; set; } //comes from user table
+        public UnregisteredUserType UserType { get; set; }
+    }
+}
